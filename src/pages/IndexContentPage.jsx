f04460045842bacd08009910b4f3084ebe3c8f54
Chris Taylor <134958907+cmtayl0r@users.noreply.github.com>
2024-08-28 22:37:40 +0200

    add index page styles

--- src/pages/IndexContentPage.jsx
+++ src/pages/IndexContentPage.jsx
@@ -6,15 +6,18 @@ function IndexContentPage() {
       <section className="index-wrapper">
         <div className="index-cur-container">
           <header className="title-block">
-            <h5>Section / 1 Columns</h5>
+            <h5>Container</h5>
           </header>
           <div className="index-sub-container col-1">
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam
-              ab neque voluptatibus, tenetur minima atque. Ducimus, voluptatem!
-              Porro, facere maiores. Ex consectetur cupiditate eligendi aperiam
-              animi odio omnis itaque voluptates!
-            </p>
+            <div>
+              <p>1 Col</p>
+              <small>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Porro,
+                earum commodi repellat mollitia impedit voluptate voluptatum
+                nisi esse, necessitatibus ipsum sit. Est tempora, maxime
+                architecto voluptate adipisci maiores nostrum sequi.
+              </small>
+            </div>
           </div>
         </div>
       </section>
@@ -22,21 +25,27 @@ function IndexContentPage() {
       <section className="index-wrapper">
         <div className="index-cur-container">
           <header className="title-block">
-            <h5>Section / 2 Columns</h5>
+            <h5>Container</h5>
           </header>
           <div className="index-sub-container col-2">
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam
-              ab neque voluptatibus, tenetur minima atque. Ducimus, voluptatem!
-              Porro, facere maiores. Ex consectetur cupiditate eligendi aperiam
-              animi odio omnis itaque voluptates!
-            </p>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam
-              ab neque voluptatibus, tenetur minima atque. Ducimus, voluptatem!
-              Porro, facere maiores. Ex consectetur cupiditate eligendi aperiam
-              animi odio omnis itaque voluptates!
-            </p>
+            <div>
+              <p>2 Cols</p>
+              <small>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Porro,
+                earum commodi repellat mollitia impedit voluptate voluptatum
+                nisi esse, necessitatibus ipsum sit. Est tempora, maxime
+                architecto voluptate adipisci maiores nostrum sequi.
+              </small>
+            </div>
+            <div>
+              <p>2 Cols</p>
+              <small>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Porro,
+                earum commodi repellat mollitia impedit voluptate voluptatum
+                nisi esse, necessitatibus ipsum sit. Est tempora, maxime
+                architecto voluptate adipisci maiores nostrum sequi.
+              </small>
+            </div>
           </div>
         </div>
       </section>
@@ -44,27 +53,36 @@ function IndexContentPage() {
       <section className="index-wrapper">
         <div className="index-cur-container">
           <header className="title-block">
-            <h5>Section / 3 Columns</h5>
+            <h5>Container</h5>
           </header>
           <div className="index-sub-container col-3">
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam
-              ab neque voluptatibus, tenetur minima atque. Ducimus, voluptatem!
-              Porro, facere maiores. Ex consectetur cupiditate eligendi aperiam
-              animi odio omnis itaque voluptates!
-            </p>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam
-              ab neque voluptatibus, tenetur minima atque. Ducimus, voluptatem!
-              Porro, facere maiores. Ex consectetur cupiditate eligendi aperiam
-              animi odio omnis itaque voluptates!
-            </p>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam
-              ab neque voluptatibus, tenetur minima atque. Ducimus, voluptatem!
-              Porro, facere maiores. Ex consectetur cupiditate eligendi aperiam
-              animi odio omnis itaque voluptates!
-            </p>
+            <div>
+              <p>3 Cols</p>
+              <small>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Porro,
+                earum commodi repellat mollitia impedit voluptate voluptatum
+                nisi esse, necessitatibus ipsum sit. Est tempora, maxime
+                architecto voluptate adipisci maiores nostrum sequi.
+              </small>
+            </div>
+            <div>
+              <p>3 Cols</p>
+              <small>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Porro,
+                earum commodi repellat mollitia impedit voluptate voluptatum
+                nisi esse, necessitatibus ipsum sit. Est tempora, maxime
+                architecto voluptate adipisci maiores nostrum sequi.
+              </small>
+            </div>
+            <div>
+              <p>3 Cols</p>
+              <small>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Porro,
+                earum commodi repellat mollitia impedit voluptate voluptatum
+                nisi esse, necessitatibus ipsum sit. Est tempora, maxime
+                architecto voluptate adipisci maiores nostrum sequi.
+              </small>
+            </div>
           </div>
         </div>
       </section>
